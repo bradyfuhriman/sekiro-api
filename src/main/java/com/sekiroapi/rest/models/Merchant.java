563665bbc904c40e39b1a1c5b3a2275067fdca93
Braden Fuhriman <bradywf18@gmail.com>
2024-07-20 01:30:46 -0400

    removed primary key generation type

--- src/main/java/com/sekiroapi/rest/models/Merchant.java
+++ src/main/java/com/sekiroapi/rest/models/Merchant.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -19,9 +17,7 @@ import lombok.Data;
 @Table(name = "merchants")
 public class Merchant {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    @Id private String id;
     private String name;
     private String location;
     
